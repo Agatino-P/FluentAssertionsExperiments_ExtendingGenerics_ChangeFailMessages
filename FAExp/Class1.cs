cb29e5125d7f07a727a98809683890a44dea5aaf
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2020-12-19 00:13:57 +0100

    Add project files.

---
+++ FAExp/Class1.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAExp
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void AssertIt()
+        {
+            int a = 1;
+            int b = 2;
+            a.Should().Be2(b, $"{nameof(a)}={a}, != {nameof(b)}={b}");
+            a.Should().Be(b,"*{0}={1}*", nameof(b),b);
+            a.Should().Be(b);
+        }
+    }
+}
---
+++ FAExp/FAExt.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using FluentAssertions.Primitives;
+using FluentAssertions.Numeric;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions.Execution;
+using FluentAssertions.Common;
+
+namespace FAExp
+{
+    public static class FAExt
+    {
+
+        public static AndConstraint<NumericAssertions<T>> Be2<T>(
+            this NumericAssertions<T> numAssert, T expected, string fail = "", params object[] failArgs)  
+            where T : struct, IComparable<T>
+        {
+            Execute.Assertion
+                .ForCondition(numAssert.Subject.IsSameOrEqualTo(expected))
+                //.BecauseOf(because, becauseArgs)
+                .FailWith(fail, failArgs);
+            
+            return new AndConstraint<NumericAssertions<T>>((NumericAssertions<T>)numAssert);
+        }
+            //public (T expected, string because = "", params object[] becauseArgs)
+    }
+}
